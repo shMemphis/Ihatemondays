c6350bbe9e0763fe7d0f849e06c57980f0a62697
Selah Hilaire <shilaire@brooklynsteamcenter.org>
2024-12-10 10:50:32 -0500

    buncha changes

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import pymysql
+
+from dynaconf import Dynaconf
+app = Flask(__name__)
+conf = Dynaconf(
+    settings_file = ('settings.toml')
+)
+
+
+def connect_db():
+    conn = pymysql.connect(
+        host ="10.100.34.80",
+        database = "shilaire_i_hate_mondays",
+        user ='shilaire',
+        password =conf.password,
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor
+    )
+
+    return conn
+
+@app.route("/")
+def home():
+    return render_template("homepage.html.jinja")
+
+@app.route('/browse')
+def product_browse():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `product` ;")
+
+    results = cursor.fetchall()
+
+    return render_template("browse.html.jinja", products =results)
